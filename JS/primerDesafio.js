49ebc97423f9eedb048969be58c1945d22331fa8
Gianluca Pasqualotto <pasqualottogianluca@gmail.com>
2022-06-02 21:50:50 -0300

    first commit

---
+++ JS/primerDesafio.js
@@ -0,0 +1,9 @@
+let numeroIngresado = prompt("Por favor, ingrese su edad")
+
+function desafio1(numeroIngresado) {
+    for (let i = 0; i < numeroIngresado; i++) {
+        console.log("Hola, tu repetición es: " + i)
+    }
+}
+
+desafio1(numeroIngresado);
